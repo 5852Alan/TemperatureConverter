63ca513e8ce582c2adddfa2d44ae341b28d1e5bf
5852Alan <alanatriano@gmail.com>
2020-06-18 23:50:06 -0700

    Added dependent spinner functionality to change between temperature and length. Added functionality from feet -> other units as well.

--- app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -17,13 +17,18 @@ public class MainActivity extends AppCompatActivity {
 
     //Vars
     private TextView result;
-    private EditText enterTemp;
+    private EditText enterVal;
     int inputtedInt = 0;
     double resultInt = 0;
-    double tempVal = 0;
-    int pos = 0;
+    int unitpos = 0;
     int val1= 0;
     int val2= 0;
+    ArrayAdapter<CharSequence> adapterChosen;
+    //Spinners
+    Spinner spinner1;
+    Spinner spinner2;
+
+
     //Toast echo
     void Toaster (CharSequence msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
@@ -32,19 +37,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Spinners defined;
+        spinner1 = findViewById(R.id.spinner1);
+        spinner2 = findViewById(R.id.spinner2);
 
-        //Spinners
-        Spinner spinner1 = findViewById(R.id.spinner1);
-        Spinner spinner2 = findViewById(R.id.spinner2);
+        //for choosing units
         Spinner spinnerUnit = findViewById(R.id.spinnerunit);
+
+        //spinnerUnit
         ArrayAdapter<CharSequence> adapterUnit = ArrayAdapter.createFromResource(this, R.array.totalunits, android.R.layout.simple_spinner_item);
+        adapterUnit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerUnit.setAdapter(adapterUnit);
 
+        //set up
+        spinnerUnit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(parent.getItemAtPosition(position).equals("Temperature")){//Temperature
+                    adapterChosen = ArrayAdapter.createFromResource(MainActivity.this, R.array.temperatureunits, android.R.layout.simple_spinner_item);
+                    spinner1.setAdapter(adapterChosen);
+                    spinner2.setAdapter(adapterChosen);
+                    unitpos = 0;
+                    Toaster("Temperature selected");
+                }
+                if (parent.getItemAtPosition(position).equals("Length")) { //Length
+                    adapterChosen = ArrayAdapter.createFromResource(MainActivity.this, R.array.lengthunits, android.R.layout.simple_spinner_item);
+                    spinner1.setAdapter(adapterChosen);
+                    spinner2.setAdapter(adapterChosen);
+                    unitpos = 1;
+                    Toaster("Length selected");
+                }
+                adapterChosen.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                adapterChosen.notifyDataSetChanged();
+                //String text = parent.getItemAtPosition(position).toString();
+                //Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+            }
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.temperatureunits, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(adapter);
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        //adapterChosen = ArrayAdapter.createFromResource(MainActivity.this, R.array.temperatureunits, android.R.layout.simple_spinner_item);
+        //unitpos = 0;
+        //spinner1.setAdapter(adapterChosen);
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -60,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
             });
 
 
-        spinner2.setAdapter(adapter);
+        //spinner2.setAdapter(adapterChosen);
         spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -77,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Vars
         result = findViewById(R.id.result);
-        enterTemp = findViewById(R.id.enterTemp);
+        enterVal = findViewById(R.id.enterVal);
         //Button
         Button conversion = findViewById(R.id.conversion);
 
@@ -85,53 +124,85 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                if(enterTemp.getText().toString().trim().length() > 0){
-                    String inputtedString = enterTemp.getText().toString();
+                if(enterVal.getText().toString().trim().length() > 0){
+                    String inputtedString = enterVal.getText().toString();
                     inputtedInt = Integer.parseInt(inputtedString);
                     resultInt=0;
                     //positions
-                    if(val1 == 0){   //fahrenheit
-                        if(val2 == 0){ //fahrenheit
-                            resultInt = inputtedInt;
-                            Toaster(resultInt + " F* = " + inputtedInt + " F*.");
+                    if(unitpos == 0) { //temp
+                        if (val1 == 0) {   //fahrenheit
+                            if (val2 == 0) { //fahrenheit
+                                resultInt = inputtedInt;
+                                Toaster(resultInt + " F* = " + inputtedInt + " F*.");
+                            }
+                            else if (val2 == 1) { //celsius
+                                resultInt = (double) (inputtedInt - 32) * 5 / 9;
+                                //resultInt = (double)
+                                Toaster(resultInt + " C* = " + inputtedInt + " F*.");
+                            }
+                            else if (val2 == 2) { //kelvin
+                                resultInt = (resultInt = (double) (inputtedInt - 32) * 5 / 9) + 273.15;
+                                Toaster(resultInt + " K* = " + inputtedInt + " F*.");
+                            }
                         }
-                        if(val2 == 1){ //celsius
-                            resultInt = (double)(inputtedInt - 32) * 5 / 9;
-                            //resultInt = (double)
-                            Toaster(resultInt + " C* = " + inputtedInt + " F*.");
+                        if (val1 == 1) {   //celsius
+                            if (val2 == 0) { //fahrenheit
+                                resultInt = (inputtedInt * 9 / 5) + 32;
+                                Toaster(resultInt + " F* = " + inputtedInt + " C*.");
+                            }
+                            else if (val2 == 1) { //celsius
+                                resultInt = inputtedInt;
+                                Toaster(resultInt + " C* = " + inputtedInt + " C*.");
+                            }
+                            else if (val2 == 2) { //kelvin
+                                resultInt = inputtedInt + 273.15;
+                                Toaster(resultInt + " K = " + inputtedInt + " C*.");
+                            }
                         }
-                        if(val2 == 2){ //kelvin
-                            resultInt = (resultInt = (double)(inputtedInt - 32) * 5 / 9) + 273.15;
-                            Toaster(resultInt + " K* = " + inputtedInt + " F*.");
+                        if (val1 == 2) {   //kelvin
+                            if (val2 == 0) { //fahrenheit
+                                resultInt = ((inputtedInt - 273.15) * 9 / 5) + 32;
+                                Toaster(resultInt + " F* = " + inputtedInt + " K.");
+                            }
+                            else if (val2 == 1) { //celsius
+                                resultInt = inputtedInt - 273.15;
+                                Toaster(resultInt + " C* = " + inputtedInt + " K.");
+                            }
+                            else if (val2 == 2) { //kelvin
+                                resultInt = inputtedInt;
+                                Toaster(resultInt + " K = " + inputtedInt + " K.");
+                            }
                         }
+
                     }
-                    if(val1 == 1){   //celsius
-                        if(val2 == 0){ //fahrenheit
-                            resultInt = (inputtedInt*9/5)+32;
-                            Toaster(resultInt + " F* = " + inputtedInt + " C*.");
+                    else if (unitpos == 1){ //length
+                        if(val1 == 0){ //feet
+                            if(val2 == 0) { //feet
+                                resultInt = inputtedInt;
+                                Toaster(resultInt + " ft = " + inputtedInt + " ft.");
+                            }
+                            else if(val2 == 1) { //mile
+                                resultInt = (double) inputtedInt / 5280;
+                                Toaster(resultInt + " mi = " + inputtedInt + " ft.");
+                            }
+                            else if(val2 == 2) { //yard
+                                resultInt = (double) inputtedInt / 3;
+                                Toaster(resultInt + " yd = " + inputtedInt + " ft.");
+                            }
+                            else if(val2 == 3) { //inch
+                                resultInt = (double) inputtedInt * 12;
+                                Toaster(resultInt + " in = " + inputtedInt + " ft.");
+                            }
+                            else if(val2 == 4) { //meters
+                                resultInt = (double) inputtedInt / 3.280839895;
+                                Toaster(resultInt + " m = " + inputtedInt + " ft.");
+                            }
+                            else if(val2 == 5) { //km
+                                resultInt = (double) inputtedInt / 3280.839895;
+                                Toaster(resultInt + " m = " + inputtedInt + " ft.");
+                            }
                         }
-                        if(val2 == 1){ //celsius
-                            resultInt = inputtedInt;
-                            Toaster(resultInt + " C* = " + inputtedInt + " C*.");
-                        }
-                        if(val2 == 2){ //kelvin
-                            resultInt = inputtedInt + 273.15;
-                            Toaster(resultInt + " K = " + inputtedInt + " C*.");
-                        }
-                    }
-                    if(val1 == 2){   //kelvin
-                        if(val2 == 0){ //fahrenheit
-                            resultInt = (double)((inputtedInt-273.15)* 9/5)+32;
-                            Toaster(resultInt + " F* = " + inputtedInt + " K.");
-                        }
-                        if(val2 == 1){ //celsius
-                            resultInt = inputtedInt-273.15;
-                            Toaster(resultInt + " C* = " + inputtedInt + " K.");
-                        }
-                        if(val2 == 2){ //kelvin
-                            resultInt = inputtedInt;
-                            Toaster(resultInt + " K = " + inputtedInt + " K.");
-                        }
+
                     }
                     result.setText(String.valueOf(resultInt));
                 }
